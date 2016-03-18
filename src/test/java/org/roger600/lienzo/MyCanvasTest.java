e29c54d919f10e54c48237040c81b6a8a85a2acb
Roger Martinez <roger600@gmail.com>
2016-03-18 21:33:13 +0100

    tests

--- src/test/java/org/roger600/lienzo/MyCanvasTest.java
+++ src/test/java/org/roger600/lienzo/MyCanvasTest.java
@@ -2,24 +2,19 @@ package org.roger600.lienzo;
 
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
-import com.google.gwtmockito.GwtMock;
-import com.google.gwtmockito.GwtMockito;
-import com.google.gwtmockito.GwtMockitoTestRunner;
-import com.google.gwtmockito.fakes.FakeProvider;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
 import static org.mockito.Mockito.when;
 
-@RunWith(GwtMockitoTestRunner.class)
+// @RunWith(GwtMockitoTestRunner.class)
 public class MyCanvasTest {
 
     @Mock
     Canvas canvas;
 
-    @GwtMock
+    // @GwtMock
     Context2d context2d;
 
     private MyCanvas myCanvas;
@@ -28,12 +23,12 @@ public class MyCanvasTest {
     public void setup() {
         when(canvas.getContext2d()).thenReturn(context2d);
 
-        GwtMockito.useProviderForType(Canvas.class, new FakeProvider<Canvas>() {
+        /*GwtMockito.useProviderForType(Canvas.class, new FakeProvider<Canvas>() {
             @Override
             public Canvas getFake(Class<?> aClass) {
                 return canvas;
             }
-        });
+        });*/
 
         myCanvas = new MyCanvas();
     }
--- src/test/java/org/roger600/lienzo/MyLienzoTest.java
+++ src/test/java/org/roger600/lienzo/MyLienzoTest.java
@@ -1,30 +1,11 @@
 package org.roger600.lienzo;
 
-import com.ait.lienzo.client.core.shape.ContainerNode;
-import com.ait.lienzo.client.core.shape.Node;
-import com.ait.lienzo.client.core.shape.Shape;
-import com.ait.tooling.nativetools.client.NArrayBaseJSO;
-import com.ait.tooling.nativetools.client.NObjectBaseJSO;
-import com.ait.tooling.nativetools.client.NObjectJSO;
-import com.ait.tooling.nativetools.client.collection.NFastArrayList;
-import com.ait.tooling.nativetools.client.collection.NFastStringMap;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwtmockito.GwtMockitoTestRunner;
-import com.google.gwtmockito.WithClassesToStub;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(GwtMockitoTestRunner.class) 
-@WithClassesToStub({NObjectBaseJSO.class, 
-        NObjectJSO.class, 
-        NArrayBaseJSO.class, 
-        NFastArrayList.class,
-        NFastStringMap.class,
-        Node.class,
-        Shape.class,
-        ContainerNode.class,
-        RootPanel.class})
+@RunWith(PowerMockRunner.class)
 public class MyLienzoTest {
     
     private MyLienzo myLienzo;
