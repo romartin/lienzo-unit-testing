47bcde4b643ddfe37aaaf1ad3cbc3658e168f65c
Roger Martinez <roger600@gmail.com>
2016-03-19 02:53:54 +0100

    js tests

--- src/main/java/org/roger600/lienzo/js/MyJSObject.java
+++ src/main/java/org/roger600/lienzo/js/MyJSObject.java
@@ -8,7 +8,7 @@ public class MyJSObject extends com.google.gwt.core.client.JavaScriptObject {
         return JavaScriptObject.createObject().cast();
     }
     
-    public MyJSObject() {
+    protected MyJSObject() {
     }
 
     public final native void put(String name, String value)
